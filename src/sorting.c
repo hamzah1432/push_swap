e64a537130b084e890d9b8378965372a326c49d2
hamzah1432 <halmuhesn@gmail.com>
2025-02-23 01:32:49 +0300

    add stack normalization function; prepare stack for sorting

--- src/sorting.c
+++ src/sorting.c
@@ -6,11 +6,46 @@
 /*   By: halmuhis <halmuhis@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 21:01:15 by halmuhis          #+#    #+#             */
-/*   Updated: 2025/02/22 19:40:03 by halmuhis         ###   ########.fr       */
+/*   Updated: 2025/02/23 01:32:00 by halmuhis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/stack.h"
+
+static void normalize_stack(t_stack *stack)
+{
+    t_node *current;
+    t_node *compare;
+    int *normalized;
+    int i;
+
+    normalized = (int *)malloc(sizeof(int) * stack->size);
+    if (!normalized)
+        return;
+    current = stack->top;
+    i = 0;
+    while (current)
+    {
+        normalized[i] = 0;
+        compare = stack->top;
+        while (compare)
+        {
+            if (current->value > compare->value)
+                normalized[i]++;
+            compare = compare->next;
+        }
+        current = current->next;
+        i++;
+    }
+    current = stack->top;
+    i = 0;
+    while (current)
+    {
+        current->value = normalized[i++];
+        current = current->next;
+    }
+    free(normalized);
+}
 
 static int get_max_bits(t_stack *stack)
 {
@@ -27,7 +62,7 @@ static int get_max_bits(t_stack *stack)
         current = current->next;
     }
     max_bits = 0;
-    while ((max >> max_bits) != 0)
+    while ((stack->size - 1) >> max_bits)
         max_bits++;
     return (max_bits);
 }
@@ -64,13 +99,15 @@ void sorting(t_stack **stack_a, t_stack **stack_b)
     if ((*stack_a)->size <= 1)
         return;
     
-    // Simple cases for small sizes
     if ((*stack_a)->size == 2)
     {
         if ((*stack_a)->top->value > (*stack_a)->top->next->value)
             ft_swap_stack(*stack_a);
         return;
     }
+    
+    // Normalize the stack before sorting
+    normalize_stack(*stack_a);
     
     // For all other cases, use radix sort
     radix_sort(*stack_a, *stack_b);
